bd724688cde35006c4bc0c8f8b5bce255a244ca2
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-10-12 07:59:48 +0100

    Modified mathangman spec

--- spec/mathangman_spec.rb
+++ spec/mathangman_spec.rb
@@ -12,11 +12,12 @@ describe Mathangman do
       expect(Mathangman::VERSION).not_to be nil
     end
     end
+
   describe "#new" do
     it 'correct type of object' do
+      allow(@game).to receive(:show_disp_menu).and_return(nil)
       expect(@game).to be_a Mathangman::Game
     end
-
   end
 
 end
